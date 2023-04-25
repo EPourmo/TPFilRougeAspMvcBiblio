523055289ab32cdf6f9e92598135e7fb47bdf3d4
LilianROUMENS <lilianroumens@gmail.com>
2023-04-25 16:55:59 +0200

    Autres controller

---
+++ AspMvcBiblio.Core/SW/IKeywordRepository.cs
@@ -0,0 +1,16 @@
+﻿using AspMvcBiblio.Entities;
+using System.Linq.Expressions;
+
+namespace AspMvcBiblio.Data
+{
+	public interface IKeywordRepository
+	{
+		Task Delete(Keyword entity);
+		Task<Keyword?> GetById(int id);
+		Task<Keyword?> GetSingle(Expression<Func<Keyword, bool>> criteria);
+		Task<Keyword> Insert(Keyword entity);
+		Task<IEnumerable<Keyword>> List(Expression<Func<Keyword, bool>> criteria);
+		Task<IEnumerable<Keyword>> ListAll();
+		Task Update(Keyword entity);
+	}
+}
---
+++ AspMvcBiblio/Controllers/KeywordsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AspMvcBiblio.Data;
+using AspMvcBiblio.Entities;
+
+namespace AspMvcBiblio.Controllers
+{
+    public class KeywordsController : Controller
+    {
+        private readonly BiblioContext _context;
+
+        public KeywordsController(BiblioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Keywords
+        public async Task<IActionResult> Index()
+        {
+              return _context.KeyWords != null ? 
+                          View(await _context.KeyWords.ToListAsync()) :
+                          Problem("Entity set 'BiblioContext.KeyWords'  is null.");
+        }
+
+        // GET: Keywords/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.KeyWords == null)
+            {
+                return NotFound();
+            }
+
+            var keyword = await _context.KeyWords
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (keyword == null)
+            {
+                return NotFound();
+            }
+
+            return View(keyword);
+        }
+
+        // GET: Keywords/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Keywords/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Word,Id")] Keyword keyword)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(keyword);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(keyword);
+        }
+
+        // GET: Keywords/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.KeyWords == null)
+            {
+                return NotFound();
+            }
+
+            var keyword = await _context.KeyWords.FindAsync(id);
+            if (keyword == null)
+            {
+                return NotFound();
+            }
+            return View(keyword);
+        }
+
+        // POST: Keywords/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Word,Id")] Keyword keyword)
+        {
+            if (id != keyword.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(keyword);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KeywordExists(keyword.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(keyword);
+        }
+
+        // GET: Keywords/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.KeyWords == null)
+            {
+                return NotFound();
+            }
+
+            var keyword = await _context.KeyWords
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (keyword == null)
+            {
+                return NotFound();
+            }
+
+            return View(keyword);
+        }
+
+        // POST: Keywords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.KeyWords == null)
+            {
+                return Problem("Entity set 'BiblioContext.KeyWords'  is null.");
+            }
+            var keyword = await _context.KeyWords.FindAsync(id);
+            if (keyword != null)
+            {
+                _context.KeyWords.Remove(keyword);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KeywordExists(int id)
+        {
+          return (_context.KeyWords?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
